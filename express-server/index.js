a4882d833a4a3db6ddbe6d16c40c1fd8c3f5d803
boris <funya7654@gmail.com>
2019-07-23 21:51:45 +0300

    connected express server

--- express-server/index.js
+++ express-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('../vkminiapp/build/static'));
+app.use('/static', express.static('../build/static'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/', 'index.html'));
